ebbef1b56999d292a9c3142a101116054733c065
RociooGaviraa <rociogaviralucena@gmail.com>
2025-06-04 16:35:32 +0200

    Proyecto Completo Terminado

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const RootLayout = () => {
-    const location = useLocation();
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, [location.pathname]);
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location.pathname]);
 
-    return (
-        <div className="min-h-screen bg-gray-50 flex flex-col">
-            <Navbar />
-            <main className="flex-1 pt-20">
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Navbar />
+      <main className="flex-1 pt-20">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default RootLayout;
